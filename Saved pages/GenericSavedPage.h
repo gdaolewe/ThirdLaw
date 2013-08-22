8fe17bdb9ac8f7de498c14bbe4fc465ad30faf0e
George Daole-Wellman <gdaolewellman@gmail.com>
2013-08-21 22:00:20 -0400

    Home page

---
+++ Saved pages/GenericSavedPage.h
@@ -0,0 +1,13 @@
+//
+//  GenericSavedPage.h
+//  Lampshade
+//
+//  Created by George Daole-Wellman on 8/21/13.
+//  Copyright (c) 2013 George Daole-Wellman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GenericSavedPage <NSObject>
+
+@end
